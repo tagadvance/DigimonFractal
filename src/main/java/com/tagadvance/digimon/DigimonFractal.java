b1000c28f62c8602123b5a790e35f7a775f40ab3
Tag <tagadvance@gmail.com>
2016-04-04 00:41:57 -0700

    initial port

---
+++ src/main/java/com/tagadvance/digimon/DigimonFractal.java
@@ -0,0 +1,38 @@
+package com.tagadvance.digimon;
+
+import static java.awt.Color.WHITE;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+import java.awt.Container;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ * 
+ * @author Tag <tagadvance@gmail.com>
+ *
+ */
+public class DigimonFractal implements Runnable {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new DigimonFractal());
+	}
+
+	public void run() {
+		String title = "Digimon Fractal";
+		JFrame window = new JFrame(title);
+		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Container container = window.getContentPane();
+		container.setBackground(WHITE);
+		double seed = 2;
+		System.out.printf("%f%n", seed);
+		JComponent fractal = new FractalComponent(seed);
+		container.add(fractal);
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+	}
+
+}
